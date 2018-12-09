13ad131f587d781ea51d7e8350402826421b15b2
amoukkadem <45723056+amoukkadem@users.noreply.github.com>
2018-12-09 01:27:37 +0100

    notification
    notification app init

---
+++ idms-notification/src/main/java/com/idms/notification/idmsnotification/IdmsNotificationApplication.java
@@ -0,0 +1,12 @@
+package com.idms.notification.idmsnotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdmsNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdmsNotificationApplication.class, args);
+	}
+}
